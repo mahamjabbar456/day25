caead543c576543b120d7f07656adbdbf60577f2
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-18 12:34:29 +0500

    i learn how to swap value without temp and how to use compound assignment operator.

---
+++ index.js
@@ -0,0 +1,31 @@
+// Question 73
+function AssignAndUpdateValue() {
+    let num = 14;
+    console.log(`The value before update is ${num}`);
+    num = 15;
+    console.log(`The value after update is ${num}`);
+}
+AssignAndUpdateValue();
+// Question 74
+function SwapValue() {
+    let a = 5;
+    let b = 10;
+    console.log(`Value before swapping \na = ${a} \n b = ${b}`);
+    [a, b] = [b, a];
+    console.log(`Value after swapping \na = ${a} \n b = ${b}`);
+}
+SwapValue();
+// Question 75
+function AssignmentOperator() {
+    let x = 4;
+    x += 2;
+    console.log(`Addition of number is ${x}`);
+    x -= 2;
+    console.log(`Subtraction of number is ${x}`);
+    x *= 2;
+    console.log(`Multiplication of number is ${x}`);
+    x /= 2;
+    console.log(`Division of number is ${x}`);
+}
+AssignmentOperator();
+export {};
